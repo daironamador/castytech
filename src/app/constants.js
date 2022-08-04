120b29c911aa85abfcffa04f993d0bb841bf32f3
Dairon <daironamador016@gmail.com>
2022-08-04 11:31:43 -0400

    casty tech

--- src/app/constants.js
+++ src/app/constants.js
@@ -1,21 +1,21 @@
 export const INFORMATION = {
   avatar:
-    "https://firebasestorage.googleapis.com/v0/b/pompococina.appspot.com/o/Imagenes%2Flogo.jpg?alt=media&token=5c0264a5-7143-4534-bfac-90fccd87168f",
+    "https://firebasestorage.googleapis.com/v0/b/castytech-12d95.appspot.com/o/Logo%2Fperfilct.png?alt=media&token=cc8fdfc2-f471-4d78-9cd7-51ba8b32c846",
   banner:
-    "https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-  title: "La Pompo Cocina",
-  description: "Los sabores que te harán sentir bien 🍔🍕🌉",
+    "https://static.vecteezy.com/system/resources/previews/000/694/589/non_2x/abstract-technology-banner-blue-background-vector.jpg",
+  title: "Casty Tech",
+  description: "Acesorios Móviles y Más 📲📱📳🖨",
   phone: "18097784474",
   sheet:
-    "https://docs.google.com/spreadsheets/d/e/2PACX-1vQNSybYozipg3yLNQYJW0pO0UivPnkEF1axUx-bOpwMBLqkNrKD6JWRdcdOu0vmq86XHY04ylHC0vDT/pub?output=csv",
-  color: "teal",
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vToQ4vZ8yJ8IFGL457k-CpFqygIPtcsaVh3fnMYQOX-Gk0iTvQTPQgSZnl3rOBDvjoj5ib9Ww38KYJb/pub?output=csv",
+  color: "blue",
   social: [
     {
-      url: "https://www.instagram.com/la_pompococina/",
+      url: "https://www.instagram.com/castytechaccesrios/",
       name: "instagram",
     },
     {
-      url: "https://wa.me/18097784474",
+      url: "https://wa.me/18095245382",
       name: "whatsapp",
     },
   ],
